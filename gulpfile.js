7e97946a7ed53d04745c6a8466c6576962564173
mtinner <m1tinner@gmail.com>
2016-06-21 16:07:18 +0200

    add transpile task

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,14 @@
 var gulp = require('gulp'),
     connect = require('gulp-connect'),
     clean = require('gulp-clean'),
+    ts = require('gulp-typescript'),
+    tsProject = ts.createProject("tsconfig.json"),
     runSequence = require('run-sequence');
 
 
 gulp.task('default', function (callback) {
-  runSequence('cleanDist',
+  runSequence(
+      ['cleanDist','transpile'],
       ['copyApp', 'connectDist'],
       callback
   );
@@ -16,6 +19,12 @@ gulp.task('cleanDist', function () {
       .pipe(clean());
 });
 
+gulp.task('transpile', function () {
+  return tsProject.src()
+      .pipe(ts(tsProject))
+      .js.pipe(gulp.dest('.dist/webapp/app'));
+});
+
 gulp.task('connectDist', function () {
   return connect.server({
     root: ['node_modules', '.dist/webapp'],
@@ -24,6 +33,14 @@ gulp.task('connectDist', function () {
 });
 
 gulp.task('copyApp', function () {
+  return gulp.src([
+        './src/main/webapp/**',
+        '!./src/main/webapp/**/*.ts'
+      ], {base: './src/main/webapp'})
+      .pipe(gulp.dest('.dist/webapp'));
+});
+
+gulp.task('copyScripts', function () {
   return gulp.src([
         './src/main/webapp/**'
       ], {base: './src/main/webapp'})
