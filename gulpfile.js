4374b0d3346b666de5446f1412185c609f29f175
mtinner <m1tinner@gmail.com>
2016-06-21 15:00:41 +0200

    add copy task

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,20 @@ var gulp = require('gulp'),
     connect = require('gulp-connect');
 
 
-gulp.task('default', ['connectDist']);
+gulp.task('default', ['copyApp','connectDist']);
 
 
 gulp.task('connectDist', function () {
   return connect.server({
-    root: ['node_modules','src/main/webapp'],
+    root: ['node_modules','.dist/webapp'],
     port: 9000
   });
+});
+
+gulp.task('copyApp', function () {
+  return gulp.src([
+        './src/main/webapp/**'
+      ],{base: './src/main/webapp'})
+      .pipe(gulp.dest('.dist/webapp'));
 });
 
