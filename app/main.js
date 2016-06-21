1b135d7f6bfc75c75e09aec196a30c2d408227af
mtinner <m1tinner@gmail.com>
2016-06-21 10:10:03 +0200

    remove generated files

--- app/main.js
+++
@@ -1,4 +0,0 @@
-var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
-var app_components_1 = require('./app.components');
-platform_browser_dynamic_1.bootstrap(app_components_1.AppComponent);
-//# sourceMappingURL=main.js.map
