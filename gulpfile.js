3b8510aa0da5d49bdc5ce8310f46619da07a21d0
mtinner <m1tinner@gmail.com>
2016-06-21 15:12:38 +0200

    add clean and sequence task

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,24 @@
 var gulp = require('gulp'),
-    connect = require('gulp-connect');
+    connect = require('gulp-connect'),
+    clean = require('gulp-clean'),
+    runSequence = require('run-sequence');
 
 
-gulp.task('default', ['copyApp','connectDist']);
+gulp.task('default', function (callback) {
+  runSequence('cleanDist',
+      ['copyApp', 'connectDist'],
+      callback
+  );
+});
 
+gulp.task('cleanDist', function () {
+  return gulp.src('.dist', {read: false})
+      .pipe(clean());
+});
 
 gulp.task('connectDist', function () {
   return connect.server({
-    root: ['node_modules','.dist/webapp'],
+    root: ['node_modules', '.dist/webapp'],
     port: 9000
   });
 });
@@ -15,7 +26,7 @@ gulp.task('connectDist', function () {
 gulp.task('copyApp', function () {
   return gulp.src([
         './src/main/webapp/**'
-      ],{base: './src/main/webapp'})
+      ], {base: './src/main/webapp'})
       .pipe(gulp.dest('.dist/webapp'));
 });
 
